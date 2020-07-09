2403414fddb7de791c452c2155c91bd1f7caa85c
Suleman Anwar <suleman.anwar.424@gmail.com>
2020-07-09 10:30:50 +0500

    first commit

---
+++ 1st Database.sql
@@ -0,0 +1,9 @@
+create database myFirstDatabase;
+use myFirstDatabase;
+CREATE TABLE ME (
+    ID int,
+    firstName nchar  
+);
+Select * FROM ME;
+INSERT INTO ME
+VALUES (1,'S');
---
+++ SQLQuery2.sql
@@ -0,0 +1,3 @@
+
+SELECT * FROM [test].[dbo].[User]
+SELECT webId,firstName FROM [test].[dbo].[User]
